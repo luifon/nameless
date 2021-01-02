8a91f21a8a00751318fc60b0922457130378b1b8
LF Consalter <luifon@alunos.utfpr.edu.br>
2021-01-02 17:50:49 -0300

    axios library added, media types validated

--- files/fileUpload.js
+++ files/fileUpload.js
@@ -3,8 +3,19 @@ const path = require('path');
 
 module.exports = (filePath, fileName, imageCreatedCallback) => {
     const type = path.extname(filePath);
-    const newPath = `./assets/images/${fileName}${type}`;
-    fs.createReadStream(filePath)
-        .pipe(fs.createWriteStream(newPath))
-        .on('finish', () => imageCreatedCallback(newPath));
+
+    const validTypes = ['jpg', 'png', 'jpeg'];
+
+    const isTypeValid = validTypes.indexOf(type.substr(1)) !== -1;
+
+    if (!isTypeValid) {
+        const message = 'Error: Invalid media type.';
+        console.log('Error: Invalid media type.');
+        imageCreatedCallback(message);
+    } else {
+        const newPath = `./assets/images/${fileName}${type}`;
+        fs.createReadStream(filePath)
+            .pipe(fs.createWriteStream(newPath))
+            .on('finish', () => imageCreatedCallback(false, newPath));
+    }
 };
--- models/patients.js
+++ models/patients.js
@@ -6,17 +6,21 @@ class Patient {
     create(patient, res) {
         const sql = 'INSERT INTO tb_patients SET ?';
 
-        fileUpload(patient.image, patient.name, (newPath) => {
-            const newPatient = { name: patient.name, image: newPath };
+        fileUpload(patient.image, patient.name, (errorMessage, newPath) => {
+            if (errorMessage) {
+                res.status(400).json({ message: errorMessage });
+            } else {
+                const newPatient = { name: patient.name, image: newPath };
 
-            connection.query(sql, newPatient, (err) => {
-                if (err) {
-                    console.log(err);
-                    res.status(400).json(err);
-                } else {
-                    res.status(200).json(newPatient);
-                }
-            });
+                connection.query(sql, newPatient, (err) => {
+                    if (err) {
+                        console.log(err);
+                        res.status(400).json(err);
+                    } else {
+                        res.status(200).json(newPatient);
+                    }
+                });
+            }
         });
     }
 }
